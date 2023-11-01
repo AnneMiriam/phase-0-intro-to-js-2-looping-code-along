0b887ce6e254fb657dd42b69e4d10a53f757b036
Anne Hastings <anne.miriam@gmail.com>
2023-11-01 16:14:12 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, event) {
+    let thanks = [];
+    for (let i = 0; i < cards.length; i++){
+        thanks.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+    return thanks;
+};
+
+function countDown(x){
+    let i = x;
+    while (i >= 0){
+        console.log(i--);
+    }
+}
